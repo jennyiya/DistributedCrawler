756aad9899a589630eeb9e4d007dc21ff8251258
jennyiya <47060591+jennyiya@users.noreply.github.com>
2020-09-08 17:16:20 +0800

    Create config.go

---
+++ config/config.go
@@ -0,0 +1,16 @@
+
+ParseCity = "ParseCity"
+ParseCityList = "ParseCityList"
+ParseProfile = "parseProfile"
+NilParser = "NilParser"
+
+//Service ports
+ItemSaverPort = 1234
+WorkerPort0 = 9000
+
+//ElasticSearch
+ElasticIndex = "dating_profile"
+
+//RPC Endpoints
+ItemSaverRpc = "ItmeSaverService.Save"
+CrawlerServiceRpc = "CrawlerService.Process"
